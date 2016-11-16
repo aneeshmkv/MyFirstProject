8a8d9a3ceda16a30ea4c475b3a004470cdc2524a
aneesh k <aneesh.k@razorthink.net>
2016-11-16 16:22:18 +0530

    Initial commit

---
+++ src/MyPackage/MyClass.java
@@ -0,0 +1,12 @@
+package MyPackage;
+
+import java.net.SocketPermission;
+
+/**
+ * Created by aneesh on 16/11/16.
+ */
+public class MyClass {
+    public static void main(String[] args) {
+        System.out.println("My name is aneesh");
+    }
+}
